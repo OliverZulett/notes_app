0642c4eb057f4db617103e130b8cfae1bb9103ae
oliverzulett <oliverzulett@gmail.com>
2023-11-29 23:20:01 -0400

    project structure

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<!-- <script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script> -->
+<template>
+  <div class="p-3">
+    <div class="navbar bg-primary text-primary-content rounded-box">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">My Notes</a>
+      </div>
+      <div class="flex-none gap-2">
+        <div class="form-control">
+          <input type="text" placeholder="Search Note" class="input bg-primary input-bordered w-24 md:w-auto" />
+        </div>
+        <div class="dropdown dropdown-end">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+              <img alt="Tailwind CSS Navbar component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </div>
+          <ul class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/NotesView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Note from '../components/Note.vue'
+</script>
+
+<template>
+  <div class="container mx-auto my-5">
+    <div class="flex">
+      <Note />
+    </div>
+  </div>
+</template> 
