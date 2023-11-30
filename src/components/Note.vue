681f98f9223979909809a01b4aa5e25cd515462b
oliverzulett <oliverzulett@gmail.com>
2023-11-30 13:23:53 -0400

    font awesome instalation

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,20 +1,19 @@
 <template>
+  <!-- <div class="card w-96 bg-primary text-primary-content image-full">
+    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure> -->
   <div class="card w-96 bg-primary text-primary-content">
     <div class="card-body pt-5 pr-5">
       <div class="card-actions justify-end">
         <button class="btn btn-sm btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+          <font-awesome-icon icon="fa-solid fa-pencil" />
         </button>
         <button class="btn btn-sm btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
+          <font-awesome-icon icon="fa-solid fa-trash-can" />
         </button>
       </div>
       <h2 class="card-title">Card title!</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
+      <p class="flex justify-end italic">Last Update: December 23</p>
     </div>
   </div>
 </template>
